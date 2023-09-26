e54ff960e3c6689b9b45e300ce5e602716ae7fd4
MaximusUnnutzus <max.wolfsberger@hotmail.com>
2023-09-26 17:03:43 +0200

    fuck this shit

--- Frontend_Task_NhS/project/logic.js
+++ Frontend_Task_NhS/project/logic.js
@@ -1,6 +1,6 @@
 var content = [
   {
-    title: "Accordion 1",
+    title: "Lorem Ipsum",
     subAccordions: [
       {
         title: "Sub 1",
@@ -20,20 +20,21 @@ var content = [
     ],
   },
   {
-    title: "Accordion 2",
+    title:
+      "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat",
     subAccordions: [
       {
-        title: "Loren Ipsum 1",
+        title: "Ipsum dolor sit amet",
         content: "lasködfj sklöadfjlöasdfjö",
         box: "jfsdklfjsdklföj eifjksdlfjsd kvmxcm,.v",
       },
       {
-        title: "Loren Ipsum 2",
+        title: "Vulputate velit esse molestie consequat",
         content: "lasködfj sklöadfjlöasdfjö",
         box: "jfsdklfjsdklföj eifjksdlfjsd kvmxcm,.v",
       },
       {
-        title: "Loren Ipsum 3",
+        title: "Iriure dolor in hendrerit",
         content: "lasködfj sklöadfjlöasdfjö",
         box: "jfsdklfjsdklföj eifjksdlfjsd kvmxcm,.v",
       },
@@ -63,12 +64,18 @@ function createAccordion(element, level) {
   panel.classList.add("panel");
 
   //define Accordion-Class-Level & append into Container or Top-Accordion
+  var accordionTitle = null;
+  var imageSrc = null;
   if (level === "toplevel") {
     accordion.classList.add("accordion");
     container.appendChild(accordion);
     container.appendChild(panel);
+    accordionTitle = document.createElement("h2");
+    imageSrc = "extras/img/secondary-plus-blue.svg";
   } else {
     accordion.classList.add("sub-accordion");
+    accordionTitle = document.createElement("h3");
+    imageSrc = "extras/img/secondary-plus-green.svg";
   }
 
   //Define Button-Layout
@@ -83,17 +90,22 @@ function createAccordion(element, level) {
   accordion.appendChild(row);
 
   //Fill Button
-  const accordionTitle = document.createElement("h2");
+  //const accordionTitle = document.createElement("h2");
   accordionTitle.innerHTML = element.title;
   colL.appendChild(accordionTitle);
   const accordionPlus = document.createElement("img");
-  accordionPlus.src = "extras/img/secondary-plus.svg";
+  accordionPlus.src = imageSrc;
   colR.appendChild(accordionPlus);
 
   //If Sublevel-Accordion find Parent-Panel & append Panel
   //Fill Panel with Content
   if (level === "sublevel") {
     panel.innerHTML = element.content;
+
+    const containerBox = document.createElement("div");
+    containerBox.classList.add("container-box");
+    containerBox.innerHTML = element.box;
+    panel.append(containerBox);
 
     //Search All Parent-Accordions
     const accordionList = document.querySelectorAll(".accordion");
@@ -106,16 +118,72 @@ function createAccordion(element, level) {
 
   //add EventListener
   accordion.addEventListener("click", function () {
-   //search last active Parent Accordion
-   var activeParentAccordion = document.querySelectorAll("."+accordion.classList.value+".active");
-   console.log(activeParentAccordion)
+    //search last active Parent Accordion
+    var activeParentAccordion = document.querySelectorAll(
+      "." + accordion.classList.value + ".active"
+    );
 
-   accordion.nextElementSibling.classList.toggle("active")
-   accordion.classList.toggle("active")
+    if (accordion.classList.value === "sub-accordion") {
+      console.log("hello");
+      accordion.style.borderBottom = "none";
+    }
 
-   activeParentAccordion.forEach((accordion)=>{
-     accordion.classList.remove("active")
-     accordion.nextElementSibling.classList.remove("active")
-   })
+    //Add / remove Active Class from same Accordion
+    accordion.nextElementSibling.classList.toggle("active");
+    accordion.classList.toggle("active");
+
+    accordion.nextElementSibling
+      .querySelectorAll(".sub-accordion.active")
+      .forEach((element) => {
+        console.log("adding bottom 1");
+        element.classList.remove("active");
+        element.nextElementSibling.classList.remove("active");
+        element.style.borderBottom = "1px solid  #c5c5c5";
+      });
+
+    //add the Button Spin Animation
+    if (
+      accordion.querySelector("img").classList.value === "" ||
+      accordion.querySelector("img").classList.value === "zeroRotate"
+    ) {
+      console.log("img");
+      accordion.querySelector("img").classList.add("fortyFiveRotate");
+    } else {
+      accordion.querySelector("img").classList.remove("fortyFiveRotate");
+      accordion.querySelector("img").classList.add("zeroRotate");
+    }
+
+    //Remove Active Class from previous Accordion
+    activeParentAccordion.forEach((accordion) => {
+      console.log("adding bottom 2");
+
+      accordion.classList.remove("active");
+      accordion.nextElementSibling.classList.remove("active");
+      accordion.querySelector("img").classList.remove("fortyFiveRotate");
+      accordion.querySelector("img").classList.add("zeroRotate");
+      accordion.style.borderBottom = "1px solid  #c5c5c5";
+      console.log("Removed Active Class From Previous");
+    });
+
+    //Remove Active Class from Child Accordions if switched to another Accordion
+    if (activeParentAccordion.length > 0) {
+      if (accordion.classList.value === "accordion active") {
+        console.log(activeParentAccordion[0]);
+        let childAccordions =
+          activeParentAccordion[0].nextElementSibling.querySelectorAll(
+            ".sub-accordion.active"
+          );
+
+        childAccordions[0].classList.remove("active");
+
+        childAccordions[0].nextElementSibling.classList.remove("active");
+        childAccordions[0].style.borderBottom = "1px solid  #c5c5c5";
+
+        childAccordions[0]
+          .querySelector("img")
+          .classList.remove("fortyFiveRotate");
+        childAccordions[0].querySelector("img").style.rotate(0);
+      }
+    }
   });
 }
